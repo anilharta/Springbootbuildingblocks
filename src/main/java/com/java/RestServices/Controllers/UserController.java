e0868e5404d50088ad456813c5d2d0ff889c7fe1
anil.harta <anil.harta@gmail.com>
2020-06-07 23:48:36 +0530

    Swagger Inplementation

--- src/main/java/com/java/RestServices/Controllers/UserController.java
+++ src/main/java/com/java/RestServices/Controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,6 +29,7 @@ import com.java.RestServices.Services.UserService;
 
 @RestController
 @Validated
+@RequestMapping(value = "/usersr")
 public class UserController {
 
 	@Autowired
